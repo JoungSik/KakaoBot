e6b34e4a2bbf84fb163924b00854b1446f17f267
JoungSik <tjstlr2010@gmail.com>
2021-09-25 17:14:25 +0900

    [Change] User model 테스트 코드 변경

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,15 +6,20 @@ RSpec.describe User, type: :model do
   end
 
   describe 'validate' do
-    it 'Email has already been taken' do
-      user = User.create({ email: @user.email, name: "example2", password: @user.password })
+    it 'presence' do
+      user = User.create({})
       user.validate
-      expect(user.errors.full_messages).to include("Email has already been taken")
+
+      expect(user.errors.full_messages).to include("Email can't be blank")
+      expect(user.errors.full_messages).to include("Password can't be blank")
+      expect(user.errors.full_messages).to include("Name can't be blank")
     end
 
-    it 'Name has already been taken' do
-      user = User.create({ email: "example2@example.com", name: @user.name, password: @user.password })
+    it 'uniqueness' do
+      user = User.create({ email: @user.email, name: @user.name, password: @user.password })
       user.validate
+
+      expect(user.errors.full_messages).to include("Email has already been taken")
       expect(user.errors.full_messages).to include("Name has already been taken")
     end
   end
