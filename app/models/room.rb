6963d69b3467343d503f6a58dee59247dacee3a6
JoungSik <tjstlr2010@gmail.com>
2021-10-01 02:23:42 +0900

    [Add] 출석체크 리스트를 가져오는 API 구현

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,7 +2,8 @@ class Room < ApplicationRecord
   belongs_to :client
 
   has_many :members
-  has_many :member_attendances, through: :members, source: :attendances
+  has_many :member_attendances, -> { where(due_date: Date.today).order(created_at: :asc) },
+           through: :members, source: :attendances
 
   validates_presence_of :name, :channel_id, :channel_type
   validates_uniqueness_of :name, :channel_id
