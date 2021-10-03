aa177398851b6b838ccac84a799d040d77ed9006
JoungSik <tjstlr2010@gmail.com>
2021-10-03 15:24:49 +0900

    #2 fixed. 채팅방 정보 가져오는 API 파라메터 수정

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -35,7 +35,7 @@ class RoomsController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_room
-    @room = Room.find(params[:id])
+    @room = Room.find_by_channel_id params[:id]
   end
 
   # Only allow a list of trusted parameters through.
