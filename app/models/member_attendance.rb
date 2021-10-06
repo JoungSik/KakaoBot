a32f1afd4f5d7cab870fcc966d55901d17ec2d9f
JoungSik <tjstlr2010@gmail.com>
2021-10-06 11:14:45 +0900

    fix, 처음 출석하는 유저는 query 가 없어 에러를 뱉는 이슈 수정

--- app/models/member_attendance.rb
+++ app/models/member_attendance.rb
@@ -2,4 +2,12 @@ class MemberAttendance < ApplicationRecord
   belongs_to :member
 
   validates :due_date, presence: true, uniqueness: { scope: :member }
+
+  def member_name
+    try(:name) ? name : member.name
+  end
+
+  def order_number
+    try(:order) ? order : member.room.member_attendances.index(self) + 1
+  end
 end
--- app/views/member_attendances/_member_attendance.json.jbuilder
+++ app/views/member_attendances/_member_attendance.json.jbuilder
@@ -1,3 +1,3 @@
 json.extract! member_attendance, :id, :due_date, :created_at, :updated_at
-json.sender member_attendance.name
-json.order member_attendance.order
+json.sender member_attendance.member_name
+json.order member_attendance.order_number
